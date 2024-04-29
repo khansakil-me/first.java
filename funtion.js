f6a6d26500723f6fef05dbefab5aef88faddc5c9
khansakil-me <khansakil.me@gmail.com>
2024-04-29 14:29:16 +0600

    first-java

---
+++ funtion.js
@@ -0,0 +1,45 @@
+// function alo (num1) {
+//     var value = 50;
+//     var singPiece = value / num1 ;
+//     return singPiece
+// }
+// var singarpoint = alo(5);
+// console.log(singarpoint);
+
+
+
+// function movie(){
+//     return "Din-The day";
+//     return "Poran";
+//     return "Hawa";
+//    }
+//    console.log(movie());
+
+
+
+// function findNumber(num1) {
+//     var odd =num1 % 2;
+//     if (odd === 0) {
+//         return false
+        
+//     } else {
+//         return true
+        
+//     }
+    
+    
+// }
+// var tf = findNumber(50);
+// console.log(tf);
+function print(a, b, c){
+    return a+2;
+    return a*b;
+    return b*c+a;
+   }
+   console.log(print(1,2,3))
+
+
+
+   const peopleName = 'Ayan' ;
+    
+   console.log(peopleName.toLowerCase());
---
+++ index.js
@@ -0,0 +1,21 @@
+
+
+
+
+
+var friend = 0;
+while(friend <99){
+    console.log('kick hobo na');
+    console.log(friend);
+    friend++;
+
+}
+
+
+var itemNum = [45,89,56,25,566,74,445,44,77,33,78,55]
+
+
+for(var i= 0; i<itemNum.length ; i++){
+    var item = itemNum[i];
+    console.log(item)
+}
