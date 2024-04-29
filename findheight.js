1d092884165beda530e9896b0fa4421e9ad60db0
khansakil-me <khansakil.me@gmail.com>
2024-04-29 21:24:18 +0600

    complet

---
+++ findheight.js
@@ -0,0 +1,10 @@
+function MaxInArray(number) {
+   for(let i =0 ; i<number.length; i++){
+const index =i;
+const element = number[index]
+console.log(element)
+   }
+}
+const height =[7 , 4, 67 ,89, 56]
+const tallest = MaxInArray(height);
+console.log(tallest);
---
+++ reverse.js
@@ -0,0 +1,13 @@
+function ReverseFunction(string){
+for (let i = string.length -1; i >=0 ; i--) {
+    const element = string[i];
+    console.log(element);
+    
+}
+}
+var str = 'I am good boy';
+var result= ReverseFunction(str) ;
+function add(a, b){
+    return a + b;
+   }
+   console.log(add("adam" + "eve"))
