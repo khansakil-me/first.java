de7ebeda071e6e3208080432e69ec59fbd08e02e
khansakil-me <khansakil.me@gmail.com>
2024-05-01 15:50:27 +0600

    complete java

--- foo.js
+++ foo.js
@@ -7,3 +7,21 @@ for (let i = 0; i <=50; i++) {
 }
 
 
+function woodCalculator(katPrice, chairPrice , tablePrice) {
+    const  perChair= 5;
+    const perKat = 10; 
+    const perTable =20;
+
+
+    const priceK = perKat * katPrice ;
+    const priceC = perChair * chairPrice ;
+    const priceT = perTable *tablePrice;
+
+
+    const totalPrice = priceK + priceC + priceT ;
+    return totalPrice;
+
+    
+}
+ const totalPrice = woodCalculator(0 ,0, 3)
+ console.log(totalPrice);
